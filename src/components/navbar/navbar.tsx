15dc40169b7ed247c7ca8a3b21e9a8ba031fecae
aydin-light <aidinsafarzade021@gmail.com>
2025-05-05 11:07:36 +0330

    better login

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,18 +1,33 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import "./Navbar.css";
-import { usePathname } from "next/navigation";
 
 const Navbar: React.FC = () => {
   const pathname = usePathname();
+  const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    setLoggedIn(!!user);
+  }, []);
+  const handleProfileClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!loggedIn) {
+      e.preventDefault();
+      router.push("/auth/signin");
+    }
+  };
 
   return (
     <nav className="navbar">
       <ul className="navbar-links">
         <li className={pathname === "/public/profile" ? "active" : ""}>
-          <Link href="/public/profile">👤 پروفایل</Link>
+          <Link href="/public/profile" onClick={handleProfileClick}>
+            👤 پروفایل
+          </Link>
         </li>
         <li className={pathname === "/public/cart" ? "active" : ""}>
           <Link href="/public/cart">🛒 سبد خرید</Link>
@@ -21,7 +36,7 @@ const Navbar: React.FC = () => {
           <Link href="/public/products">محصولات</Link>
         </li>
         <li className={pathname === "/public/pre-build" ? "active" : ""}>
-          <Link href="/public/pre-build">سیستم های اماده</Link>
+          <Link href="/public/pre-build">سیستم های آماده</Link>
         </li>
         <li className={pathname === "/public/about-us" ? "active" : ""}>
           <Link href="/public/about-us">درباره ما</Link>
